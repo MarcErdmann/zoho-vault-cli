74c3a440657d3957f578273664210d70f81b67c2
marc <you@example.com>
2019-11-27 08:30:26 +0100

    Try zoho oauth

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+const axios = require('axios');
 const keytar = require('keytar');
 const program = require('commander');
 
-program.version('0.0.1');
-
-program
-.command('set <value>')
-.description('set a password')
-.action((key) => {
-    keytar.setPassword('vault', 'default', '0007');
+axios.post('https://accounts.zoho.com/oauth/v3/device/code', {}, {
+    params: {
+        client_id: '1000.VQHN4RUSH8H3TSRWPNM7LOJUEJRHRH',
+        grant_type: 'device_request',
+        scope: 'ZohoVault.secrets.READ'
+    }
+}).then(response => {
+    console.log(response.data);
+}).catch(err => {
+    console.error(err);
 });
 
-program
-.command('get')
-.description('get a password')
-.action(async () => {
-    console.log(await keytar.getPassword('vault', 'default'));
-})
+// program.version('0.0.1');
 
-program.parse(process.argv);
+// program
+// .command('set <value>')
+// .description('set a password')
+// .action((key) => {
+//     keytar.setPassword('vault', 'default', '0007');
+// });
+
+// program
+// .command('get')
+// .description('get a password')
+// .action(async () => {
+//     console.log(await keytar.getPassword('vault', 'default'));
+// })
+
+// program.parse(process.argv);
